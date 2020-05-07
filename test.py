a991e6b3e667f13f8c8c30f7262afeffe56a4333
Yao He <lcxywfe@gmail.com>
2020-05-07 05:21:08 +0000

    test

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 import numpy as np
 import tensorflow as tf
+import datetime
 from model.refinenet import RefineNet
 
 refinenet = RefineNet(27)
-out = refinenet(np.random.random((1, 224, 224, 3)).astype(np.float32))
-print(out.shape)
+refinenet.compile(optimizer='adam', loss='mean_absolute_error', metrics=['accuracy'])
 
-# tf.keras.utils.plot_model(out, to_file='model.png', show_shapes=False, show_layer_names=True,
-#                 rankdir='TB', expand_nested=True, dpi=96)
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+x_train = np.random.random((2, 224, 224, 3)).astype(np.float32)
+y_train = np.random.random((2, 224, 224, 27)).astype(np.float32)
+
+x_test = np.random.random((1, 224, 224, 3)).astype(np.float32)
+y_test = np.random.random((1, 224, 224, 27)).astype(np.float32)
+
+refinenet.fit(x=x_train,
+          y=y_train,
+          epochs=5,
+          validation_data=(x_test, y_test),
+          callbacks=[tensorboard_callback])
